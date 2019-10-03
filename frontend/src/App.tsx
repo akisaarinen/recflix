f3fac9a040a7661a1629cd85318a509224b9c6ca
Aki Saarinen <aki.saarinen@gmail.com>
2019-10-03 22:59:30 +0900

    frontend: Add simple react-router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,11 +2,18 @@ import React from 'react'
 import './App.css';
 import { Home } from './components/Home'
 import { Menu } from './components/Menu'
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <Menu><Home/></Menu>
+      <Menu>
+        <Router>
+          <Route path="/" exact>
+            <Home/>
+          </Route>
+        </Router>
+      </Menu>
     </div>
   );
 }
