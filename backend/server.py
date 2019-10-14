b3a150b7da5355c2170c650dae2c78ba6a7281d7
Aki Saarinen <aki.saarinen@gmail.com>
2019-10-14 21:29:33 +0900

    backend: Add simple similarity recommender
    This is using custom data that has to be placed under data folder.

--- backend/server.py
+++ backend/server.py
@@ -1,4 +1,7 @@
+import numpy as np
 import pandas as pd
+from scipy.sparse import csr_matrix
+
 from flask import Flask, jsonify, make_response
 from flask_cors import CORS
 
@@ -7,7 +10,36 @@ app = Flask(__name__)
 # Note: no restrictions atm
 CORS(app)
 
-movies = pd.read_csv("data/movies_metadata_recflix.csv.gz")
+# Load data
+def load_sparse_csr(filename):
+  loader = np.load(filename + '.npz')
+  return csr_matrix((loader['data'], loader['indices'], loader['indptr']),
+                    shape=loader['shape'])
+
+print("Loading data:")
+print("- Cosine similarity")
+sim_cos = load_sparse_csr("data/sim_ml-latest_matrix")
+imdb_ids = pd.read_csv(
+  "data/sim_ml-latest_mapping.csv",
+  names=["sim_id", "imdb_id"],
+  header=0,
+  dtype={ "sim_id": int, 'imdb_id': str }
+)
+print("  [sim_cos.shape: %s]" % str(sim_cos.shape))
+print("  [imdb_ids.shape: %s]" % str(imdb_ids.shape))
+
+print("- Movies metadata")
+movies = pd.read_csv(
+  "data/movies_metadata_recflix.csv.gz")
+print("  [movies.shape: %s]" % str(movies.shape))
+
+print("")
+print("Processing data:")
+print("- Match and merge movies from datasets")
+imdb_ids['imdb_id'] = imdb_ids['imdb_id'].map(lambda x: "tt%s" % x)
+movies = movies.merge(imdb_ids, how='inner')
+print(movies.head(3))
+print("=> Done")
 
 @app.route('/')
 def root():
@@ -41,6 +73,20 @@ def format_movie_details(frame):
     "voteCount": str(frame["vote_count"]),
   })
 
+def recommendationSimilarItems(movie):
+  sim_id = movie['sim_id']
+  not_included = [sim_id]
+  sim_val = sim_cos[sim_id].toarray().flatten()
+  sim_df = pd.DataFrame({
+    "sim" : sim_val,
+    "sim_id": np.arange(len(sim_val)),
+  })
+  sim_df.drop(index=not_included, inplace=True)
+  sim_df = sim_df.sort_values(by='sim', ascending=False)
+  sim_df = sim_df[:15]
+  sim_df = sim_df.merge(movies)
+  return sim_df
+
 def recommendationRecentPopular():
   MIN_YEAR = 2010
   MIN_COUNT = 1000
@@ -61,14 +107,23 @@ def recommendationPersonalized():
 
 API_VERSION = "v1"
 
-@app.route('/api/%s/movie/<movieId>' % API_VERSION, methods=["GET"])
-def apiMovies(movieId):
-  movie_match = movies[movies['imdb_id'] == movieId]
+@app.route('/api/%s/movie/<imdbId>' % API_VERSION, methods=["GET"])
+def apiMovies(imdbId):
+  movie_match = movies[movies['imdb_id'] == imdbId]
   if len(movie_match) != 1:
     return make_response(jsonify({
       "match_count" : len(movie_match)
     }), 404)
   return jsonify(format_movie_details(movie_match.iloc[0]))
+
+@app.route('/api/%s/recommendation/similarItems/<imdbId>' % API_VERSION, methods=["GET"])
+def apiRecommendationSimilarItems(imdbId):
+  movie_match = movies[movies['imdb_id'] == imdbId]
+  if len(movie_match) != 1:
+    return make_response(jsonify({
+      "match_count" : len(movie_match)
+    }), 404)
+  return jsonify(list(recommendationSimilarItems(movie_match.iloc[0]).apply(format_movie, axis=1)))
 
 @app.route('/api/%s/recommendation/recentPopular' % API_VERSION, methods=["GET"])
 def apiRecommendationRecentPopular():
